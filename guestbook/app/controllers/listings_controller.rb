88f7e34e9f695d3ace42e0ea64577a5c64879d47
Etienne Mustow <etienne.o.mustow@gmail.com>
2018-05-16 16:09:07 +0000

    listings is at root, listings views created

---
+++ guestbook/app/controllers/listings_controller.rb
@@ -0,0 +1,26 @@
+class ListingsController < ApplicationController
+    def new
+    end
+    
+    def index
+        @listings = Listing.all
+    end
+    
+    def create
+         
+    @listing = Listing.new(listing_params)
+
+ 
+    @listing.save
+    redirect_to @listing
+    end
+    
+    def show
+    @listing = Listing.find(params[:id])
+    end
+    
+    private
+    def listing_params
+        params.require(:listing).permit(:name, :message)
+    end
+end
